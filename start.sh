94d163a508e7f7ebb66fbf250aca379dc0becadb
Yihang Wang <wangyihanger@gmail.com>
2026-02-06 11:47:59 +0800

    feat: Replace the `searcher` module with a `crawler` module, update CLI command names, and refactor associated tests.

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-uvx sbom-insight search-github
-uvx sbom-insight download-sbom
-uvx sbom-insight convert-sbom
+uvx sbom-insight search-github --language go --min-stars 1000
+uvx sbom-insight download-sbom --language go
+uvx sbom-insight convert-sbom --language go
+uvx sbom-insight importer --language go
+uvx sbom-insight query --language go gin
+uvx sbom-insight summarize
+uvx sbom-insight agent
